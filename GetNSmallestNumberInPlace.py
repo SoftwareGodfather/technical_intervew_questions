0cf1480c4bb3be2e6117609ef38d9a9d1ee0b022
Konstantin Verein <59009363+SoftwareGodfather@users.noreply.github.com>
2019-12-23 11:45:19 +0200

    Update GetNSmallestNumberInPlace.py

--- GetNSmallestNumberInPlace.py
+++ GetNSmallestNumberInPlace.py
@@ -16,16 +16,43 @@ def GetNSmallestNumberInPlace(arr, n):
     swap(arr, minIndex, len(arr)-i-1)
 
   return smallestList
-    
+
+###########
+
+def GetSmallestIndexUsingSet(arr, smallestIndexSet):
+  minIndex = 0
+  for i in range(0, len(arr)):
+    if (arr[i] < arr[minIndex]) and (i not in smallestIndexSet):
+      minIndex = i
+  smallestIndexSet.add(minIndex)
+  return minIndex
+
+def GetNSmallestNumberInPlaceNoArrModification(arr, n):
+  smallestIndexSet = set()
+  for i in range(0, n):
+    minIndex = GetSmallestIndexUsingSet(arr, smallestIndexSet)
+
+  res = [arr[elem] for elem in smallestIndexSet]
+  return res
+
 ########### MAIN ###########
 
 arr = [9, 4, 1, 5, 7, 2, 3, 6, 8]
-print(f'Before algorithm: {arr}')
+print(f'Before 1 algorithm: {arr}')
 print( GetNSmallestNumberInPlace(arr, 2))
-print(f'After algorithm:  {arr}')
+print(f'After 1 algorithm:  {arr} \n')
+
+arr = [9, 4, 1, 5, 7, 2, 3, 6, 8]
+print(f'Before 2 algorithm: {arr}')
+print( GetNSmallestNumberInPlaceNoArrModification(arr, 2))
+print(f'After 2 algorithm:  {arr}')
 
 ########### STDOUT ###########
 
-#Before algorithm: [9, 4, 1, 5, 7, 2, 3, 6, 8]
+#Before 1 algorithm: [9, 4, 1, 5, 7, 2, 3, 6, 8]
 #[1, 2]
-#After algorithm:  [9, 4, 8, 5, 7, 6, 3, 2, 1]
+#After 1 algorithm:  [9, 4, 8, 5, 7, 6, 3, 2, 1]
+
+#Before 2 algorithm: [9, 4, 1, 5, 7, 2, 3, 6, 8]
+#[1, 2]
+#After 2 algorithm:  [9, 4, 1, 5, 7, 2, 3, 6, 8]
