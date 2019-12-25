f70eae5bb7b1c06249404272c40a824b35cc636f
Konstantin Verein <59009363+SoftwareGodfather@users.noreply.github.com>
2019-12-25 23:21:49 +0200

    Update MeetingHourOptimization.py

--- MeetingHourOptimization.py
+++ MeetingHourOptimization.py
@@ -1,24 +1,32 @@
 
+from collections import namedtuple
+
 def getKey(meet):
     return meet.hours
 
 def SortByHours(meetings):
     return sorted(meetings, key=getKey)
 
+def MaximizeByMeetingNumber(meetings, totalHours):
+    meetHours = 0
+    meetingList = []
+    sortMeetings = SortByHours(meetings)
+    for elem in sortMeetings:
+        if(meetHours + elem.hours <= totalHours):
+            meetingList.append(elem)
+            meetHours += elem.hours
+        else:
+            break
+    return meetingList
+
 ########### MAIN ###########
 
 meeting = namedtuple('Meeting', ['name', 'hours'])
 meetings = [meeting('first',2), meeting('second',1), meeting('third',3)]
-sortMeetings = SortByHours(meetings)
 
-totalHours = 3
-meetHours = 0
-meetingList = []
-for elem in sortMeetings:
-    if(meetHours + elem.hours <= totalHours):
-        meetingList.append(elem)
-        meetHours += elem.hours
-    else:
-        break
-
+meetingList = MaximizeByMeetingNumber(meetings, 4)
 print(meetingList)
+
+########### STDOUT ###########
+
+#[Meeting(name='second', hours=1), Meeting(name='first', hours=2)]
