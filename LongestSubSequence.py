2f885b247f0cabe13fe407401fd987a5a3d724a8
Konstantin Verein <59009363+SoftwareGodfather@users.noreply.github.com>
2019-12-25 11:46:25 +0200

    Update LongestSubSequence.py

--- LongestSubSequence.py
+++ LongestSubSequence.py
@@ -1,3 +1,21 @@
+def MapLocations(myStr):
+    loc = dict()
+    for ind, elem in enumerate(myStr):
+        if elem not in loc.keys():
+            loc[elem] = [ind]
+        else:
+            loc[elem].append(ind)
+    return loc
+        
+def FindCharInStringUsingMap(myChar, mapLocations, pos):
+    if(myChar not in mapLocations.keys()):
+        return -1
+    locList = mapLocations[myChar]
+    for singleLocation in locList:
+        if singleLocation >= pos:
+            return singleLocation
+    return -1
+    
 def GetLongestString(myStr):
     maxStr = ''
     for elem in myStr:
@@ -5,26 +23,28 @@ def GetLongestString(myStr):
             maxStr =  elem
     return maxStr
 
-def FindSubstringFromPosition(strA, strB, pointA, pointB):
+def FindSubstringFromPosition(strA, strB, pointA, pointB, mapLocB):
     result = strA[pointA]
     i = pointA + 1
     j = pointB + 1
     while(j < len(strB)) and (i < len(strA)):
         currentlyLookingFor = strA[i]
         foundInStrB = strB[j:].find(currentlyLookingFor)
+        foundInStrB = FindCharInStringUsingMap(currentlyLookingFor, mapLocB, j)
         if(foundInStrB != -1):
             result += currentlyLookingFor
-            j += foundInStrB + 1
+            j = foundInStrB + 1
         i += 1
     return result
 
-def LongestSubSequence(strA, strB):
+def LongestSubSequence(strA, strB, mapLocB):
     allResults = []
     for indA, charA in enumerate(strA):
         pointA = indA
         pointB = strB.find(charA)
+        pointB = FindCharInStringUsingMap(charA, mapLocB, 0)
         if(pointB != -1):
-            res = FindSubstringFromPosition(strA, strB, pointA, pointB)
+            res = FindSubstringFromPosition(strA, strB, pointA, pointB, mapLocB)
             allResults.append(res)
     return allResults
 
@@ -33,7 +53,9 @@ def LongestSubSequence(strA, strB):
 strA = 'ABAZDC'
 strB = 'BACBAD'
 
-res = LongestSubSequence(strA, strB)
+mapLocB = MapLocations(strB)
+
+res = LongestSubSequence(strA, strB, mapLocB)
 res = GetLongestString(res)
 print(res)
 
