138b038824b98b5852278ca515ba039def2d22ff
Konstantin Verein <59009363+SoftwareGodfather@users.noreply.github.com>
2019-12-26 10:47:38 +0200

    Update K_Most_Frequent.py

--- K_Most_Frequent.py
+++ K_Most_Frequent.py
@@ -12,12 +12,15 @@ def FindKFrequentElem(my_str, k_frequent):
             heapq.heappush(res, (v, k))
         elif(v > res[0][0]):
             heapq.heappushpop(res, (v, k))
+        elif(v == res[0][0]):
+            print(f'k can not be {k_frequent}')
+            return []
 
     return res
 
 ########### MAIN ###########
 
-my_str = [1,1,1,2,2,3]
+my_str = [1,1,2,2,3]
 k_frequent = 2
 
 res = FindKFrequentElem(my_str, k_frequent)
@@ -27,5 +30,5 @@ for elem in res:
 
 ########### STDOUT ###########
 
-#2
 #1
+#2
