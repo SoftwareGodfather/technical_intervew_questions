9d328ccd3306f9499ad0bf92990e55d77fe51ff1
Konstantin Verein <59009363+SoftwareGodfather@users.noreply.github.com>
2019-12-29 20:49:42 +0200

    Update Graph_Questions.py

--- Graph_Questions.py
+++ Graph_Questions.py
@@ -1,26 +1,71 @@
-from collections import defaultdict
+import collections
 
 class Graph:
-    def __init__(self, N):
-        self.N = N
-        graph = defaultdict(list)
+    def __init__(self, V):
+        self.V = V
+        self.graph = collections.defaultdict(list)
 
-    def addEdge(self, v, w):
+    def AddEdge(self, v, w):
         self.graph[v].append(w)
         self.graph[w].append(v)
 
+    def HandleElemDict(self, elem):
+        print(f'data:{elem["data"]}, level:{elem["level"]}')
+
+    def HandleElem(self, elem):
+        print(elem)
+
+    def BFS(self, root):
+        print('BFS')
+        parent = dict()
+        queue = collections.deque()
+        queue.appendleft({'data':root,'level':0})
+        parent[root] = None
+        while queue:
+            elem = queue.pop()
+            self.HandleElemDict(elem)
+            for child in self.graph[elem["data"]]:
+                if child not in parent.keys():
+                    childLevel = elem["level"] + 1
+                    queue.appendleft({'data':child,'level':childLevel})
+                    parent[child] = elem["data"]
+
+    def DFS(self,root):
+        print('DFS')
+        parent = dict()
+        stack = []
+        stack.append(root)
+        parent[root] = None
+        while stack:
+            elem = stack.pop()
+            self.HandleElem(elem)
+            for child in self.graph[elem]:
+                if child not in parent.keys():
+                    stack.append(child)
+                    parent[child] = elem
+
 # Driver program to test above functions 
 g1 = Graph(5) 
-g1.addEdge(1, 0) 
-g1.addEdge(0, 2) 
-g1.addEdge(0, 3) 
-g1.addEdge(3, 4) 
-print(g1.isTree())
-  
-g2 = Graph(5) 
-g2.addEdge(1, 0) 
-g2.addEdge(0, 2) 
-g2.addEdge(2, 1) 
-g2.addEdge(0, 3) 
-g2.addEdge(3, 4) 
-print(g2.isTree())
+g1.AddEdge(1, 0)
+g1.AddEdge(0, 2)
+g1.AddEdge(0, 3)
+g1.AddEdge(2, 5)
+g1.AddEdge(3, 4)
+g1.AddEdge(0, 4)
+g1.BFS(0)
+g1.DFS(0)
+
+########### STDOUT ###########
+
+#BFS
+#0
+#1
+#2
+#3
+#4
+#DFS
+#0
+#3
+#4
+#2
+#1
